9871434e132088b825738226d3c63b46140fac6b
marwa <aitkhroufmarwa@gmail.com>
2025-03-01 17:03:19 +0000

    register

--- student/register.php
+++ student/register.php
@@ -38,7 +38,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="../login.css">
+
+    <link rel="stylesheet" href="register.css">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
